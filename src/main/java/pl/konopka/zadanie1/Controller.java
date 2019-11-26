5396be37f084440daa1a98a4e7cf36d23bdc6723
Marcin Konopka <marcinkonopka123@wp.pl>
2019-11-26 18:07:18 +0100

    Add controller with reverse logic

---
+++ src/main/java/pl/konopka/zadanie1/Controller.java
@@ -0,0 +1,16 @@
+package pl.konopka.zadanie1;
+
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping(value = "/{string}")
+    String reverse(@PathVariable String string) {
+        return StringUtils.reverse(string);
+    }
+
+}
